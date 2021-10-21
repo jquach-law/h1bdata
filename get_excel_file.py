3ad0c55a208a3e41e439d424cfc79d587553ae46
jquac <quachlaj@oregonstate.edu>
2021-10-20 21:14:12 -0500

    Get/parse html from ww.dol.gov and access pdf files

---
+++ get_excel_file.py
@@ -0,0 +1,34 @@
+import bs4
+import requests
+
+BASE_URL = 'https://www.dol.gov'
+
+
+# function to extract html document from given url
+def getHTMLdocument(url):
+    # request for HTML document of given url
+    response = requests.get(url)
+    # response will be provided in JSON format
+    return response.text
+
+
+url_to_scrape = 'https://www.dol.gov/agencies/eta/foreign-labor/performance'
+
+# create document
+html_document = getHTMLdocument(url_to_scrape)
+
+# create soup object
+soup = bs4.BeautifulSoup(html_document, 'html.parser')
+
+# Get to excel
+tag_element = soup.find(title='PERM Disclosure Data FY2021')
+path = tag_element['href']
+
+file_url = BASE_URL + path
+# Download file to memory
+response = requests.get(file_url)
+
+# # Save the file to disk
+# file_name = file_url.split('/')[-1]
+# with open(file_name, 'wb') as output_file:
+#     output_file.write(response.content)

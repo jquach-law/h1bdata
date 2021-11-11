2927550666b0a93cc501cf160609723110013a36
Jason Li <iverefiner@gmail.com>
2021-11-11 14:51:13 -0600

    added a input to select year and quarter to filter result so we won't have to manually hard code it every time

--- get_excel_file.py
+++ get_excel_file.py
@@ -23,25 +23,40 @@ soup = bs4.BeautifulSoup(html_document, 'html.parser')
 
 # create a variable to change search query
 
-query = 'a[href*="LCA_Disclosure_Data_FY2021_Q4"]'
+print('Enter the desired year')
+year = input()
+print('Which quarter? (Q1, Q2, Q3, Q4)')
+quarter = input()
+
+# query = 'a[href*=LCA_Disclosure_Data_FY2021_Q4]'
+query = 'a[href*=LCA_Disclosure_Data_FY' + year + '_' + quarter + ']'
+
 # query = 'a[href*="LCA_Disclosure_Data"]'
 
 # Find the links to the relevant excel files
 matching_tag_elements = soup.select(query)
 
+
 for tag_element in matching_tag_elements:
+    print(tag_element)
+
+    # print('\n')
+
     path = tag_element['href']
 
-    file_url = BASE_URL + path
-    # Download file to memory
-    response = requests.get(file_url)
+    # if '2021' and 'Q4' in path:
+    print(path)
 
-    # Create folder to hold the data
-    if not Path('data').is_dir():
-        Path('data').mkdir()
+    # file_url = BASE_URL + path
+    # # Download file to memory
+    # response = requests.get(file_url)
 
-    # Save the data in memory to disk
-    file_name = f"data/{file_url.split('/')[-1]}"
-    with open(file_name, 'wb') as output_file:
-        output_file.write(response.content)
+    # # Create folder to hold the data
+    # if not Path('data').is_dir():
+    #     Path('data').mkdir()
+
+    # # Save the data in memory to disk
+    # file_name = f"data/{file_url.split('/')[-1]}"
+    # with open(file_name, 'wb') as output_file:
+    #     output_file.write(response.content)
 

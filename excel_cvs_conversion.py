33ff1643b1176e225c3496b8edd3a1b85f9e0db5
Jason Li <iverefiner@gmail.com>
2021-11-11 15:57:39 -0600

    working on forcing user to enter correct input and streamlining file conversion names'

--- excel_cvs_conversion.py
+++ excel_cvs_conversion.py
@@ -1,6 +1,16 @@
+import get_excel_file as query
 import pandas as pd
 import csv
 
-# Convert excel to csv
-read_file = pd.read_excel (r'LCA_Disclosure_Data_FY2021_Q3.xlsx')
-read_file.to_csv (r'LCA_Disclosure_Data_FY2021_Q3.csv', index = None, header=True)
+# excel_file_name = 'LCA_Disclosure_Data_FY' + year + '_' + quarter + '.xlsx'
+# csv_file_name = 'LCA_Disclosure_Data_FY' + year + '_' + quarter + '.csv'
+
+excel_file_name = query.file_name[5:]
+csv_file_name = query.file_name[5:-4] + 'csv'
+
+# # Convert excel to csv
+# read_file = pd.read_excel (excel_file_name)
+# read_file.to_csv (csv_file_name, index = None, header=True)
+
+print(excel_file_name)
+print(csv_file_name)
--- get_excel_file.py
+++ get_excel_file.py
@@ -22,9 +22,11 @@ html_document = getHTMLdocument(url_to_scrape)
 soup = bs4.BeautifulSoup(html_document, 'html.parser')
 
 # asks the user to input year and date to filter out search query/download only one file
-print('Enter the desired year')
+while year != int or False
+print('Enter the desired year: ')
 year = input()
-print('Which quarter? (Q1, Q2, Q3, Q4)')
+while quarter != 'Q1' or 'Q2' or 'Q3' or 'Q4' or False
+print('Which quarter? (Q1, Q2, Q3, Q4): ')
 quarter = input()
 
 # query = 'a[href*=LCA_Disclosure_Data_FY2021_Q4]'
@@ -38,8 +40,10 @@ matching_tag_elements = soup.select(query)
 
 for tag_element in matching_tag_elements:
 
+    # tag_element is the entire class a tag that contains the href so this will extract the value of href
     path = tag_element['href']
 
+    # href value is the  subdirectory so it's combined with the main website URL
     file_url = BASE_URL + path
     # Download file to memory
     response = requests.get(file_url)

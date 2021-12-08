a63c23802b00b35e3a25d5460ca0aa43f4a391d1
Jason Li <iverefiner@gmail.com>
2021-12-08 11:44:09 -0600

    float tables with strings now turn strings in to NaN

--- excel_cvs_conversion.py
+++ excel_cvs_conversion.py
@@ -42,6 +42,7 @@ def data_cleaning():
                 'PW_UNIT_OF_PAY': str,
             }
         )
+        
         df = get_cleaned_dataframe(df)
     except ValueError:
         print('there was an error')
@@ -60,52 +61,61 @@ def data_cleaning():
     #df.to_csv(f'{FILENAME}.csv', index=None, header=True)
 
 def get_cleaned_dataframe(df):
+
     print('before stripping')
     print(df)
     # Get rid of all the spaces
     # Standardize casing for strings
     
-    # TODO: Change to list comprehension
-    # This doesn't work because list comprehension turns this into a list and not an object as required to mutate an entire dataframe
+    #̶ T̶O̶D̶O̶:̶ C̶h̶a̶n̶g̶e̶ t̶o̶ l̶i̶s̶t̶ c̶o̶m̶p̶r̶e̶h̶e̶n̶s̶i̶o̶n̶
+    # List comprehension below doesn't work because list comprehension turns this into a list and not an object as required to mutate an entire dataframe
     # df = [df.iloc[:, row].str.strip() for row in range(len(df.columns)) if df.iloc[:, row].dtype == 'object']
+
+    # Converting float column with strings to turn them into NaN. Otherwise, stripping will get rid of numbers in columns with strings
+    df['PREVAILING_WAGE'] = pd.to_numeric(df['PREVAILING_WAGE'], errors='coerce')
+    df['WAGE_RATE_OF_PAY_FROM'] = pd.to_numeric(df['PREVAILING_WAGE'], errors='coerce')
+    df['WAGE_RATE_OF_PAY_TO'] = pd.to_numeric(df['PREVAILING_WAGE'], errors='coerce')
 
     # Create an object out of all the columns with the dtype object
     df_object = df.select_dtypes(['object'])
+
     # Modify key values
         # Strips away leading and trailing spaces
     df[df_object.columns] = df_object.apply(lambda x: x.str.strip())
         # Title/proper casing for all the values
-        # For some reason I had to make a new object in order for it to strip AND titlecase because method chaining didn't work
-        # Without a new object, the effect of one method replaces the other
+            # For some reason I had to make a new object in order for it to strip AND titlecase because method chaining didn't work
+            # Without a new object, the effect of one method replaces the other
     stripped_df_object = df.select_dtypes(['object'])
     df[stripped_df_object.columns] = stripped_df_object.apply(lambda x: x.str.title())
 
-    print('after stripping')
+    print('after coercing strings to floats and stripping')
     print(df)
 
     # Type casting
-    df['CASE_NUMBER'] = df['CASE_NUMBER'].astype(str)
-    df['VISA_CLASS'] = df['VISA_CLASS'].astype(str)
-    df['JOB_TITLE'] = df['JOB_TITLE'].astype(str)
-    df['SOC_TITLE'] = df['SOC_TITLE'].astype(str)
-    df['FULL_TIME_POSITION'] = df['FULL_TIME_POSITION'].astype(str)
-    df['EMPLOYER_NAME'] = df['EMPLOYER_NAME'].astype(str)
-    df['EMPLOYER_CITY'] = df['EMPLOYER_CITY'].astype(str)
-    df['EMPLOYER_STATE'] = df['EMPLOYER_STATE'].astype(str)
-    df['WAGE_RATE_OF_PAY_FROM'] = df['WAGE_RATE_OF_PAY_FROM'].astype(float)
-    df['WAGE_RATE_OF_PAY_TO'] = df['WAGE_RATE_OF_PAY_TO'].astype(float)
-    df['WAGE_UNIT_OF_PAY'] = df['WAGE_UNIT_OF_PAY'].astype(str)
-    df['PREVAILING_WAGE'] = df['PREVAILING_WAGE'].astype(float)
-    df['PW_UNIT_OF_PAY'] = df['PW_UNIT_OF_PAY'].astype(str)
+    # df['CASE_NUMBER'] = df['CASE_NUMBER'].astype(str)
+    # df['VISA_CLASS'] = df['VISA_CLASS'].astype(str)
+    # df['JOB_TITLE'] = df['JOB_TITLE'].astype(str)
+    # df['SOC_TITLE'] = df['SOC_TITLE'].astype(str)
+    # df['FULL_TIME_POSITION'] = df['FULL_TIME_POSITION'].astype(str)
+    # df['EMPLOYER_NAME'] = df['EMPLOYER_NAME'].astype(str)
+    # df['EMPLOYER_CITY'] = df['EMPLOYER_CITY'].astype(str)
+    # df['EMPLOYER_STATE'] = df['EMPLOYER_STATE'].astype(str)
+    # df['WAGE_RATE_OF_PAY_FROM'] = df['WAGE_RATE_OF_PAY_FROM'].astype(float)
+    # df['WAGE_RATE_OF_PAY_TO'] = df['WAGE_RATE_OF_PAY_TO'].astype(float)
+    # df['WAGE_UNIT_OF_PAY'] = df['WAGE_UNIT_OF_PAY'].astype(str)
+    # df['PREVAILING_WAGE'] = df['PREVAILING_WAGE'].astype(float)
+    # df['PW_UNIT_OF_PAY'] = df['PW_UNIT_OF_PAY'].astype(str)
 
     # TODO: Get rid of rows if columns don't match a pattern
     # ^[^0-9].*
-    df = df.loc[
-        (df['VISA_CLASS'].str.contains(r'^[^0-9].*', regex=True, na=False))
-    ]
+    # df = df.loc[
+    #     (df['VISA_CLASS'].str.contains(r'^[^0-9].*', regex=True, na=False))
+    # ]
+    df = df.loc[(df['VISA_CLASS'].str.contains(r'^[^\d].*'))]
 
-    print('after regex')
-    print(df)
+    # print('after regex')
+    # print(df)
+
 
     # TODO: Add regex to make sure float columns don't contain strings
 

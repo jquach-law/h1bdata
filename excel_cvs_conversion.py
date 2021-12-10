321e6a7cdf96c5eb8837e9e8c10f74dde2a3bb30
Allen <allenjhuang92@gmail.com>
2021-12-10 17:58:01 -0600

    Refactored csv_to_df and get_cleaned_df functions

--- excel_cvs_conversion.py
+++ excel_cvs_conversion.py
@@ -5,7 +5,6 @@ import sqlalchemy
 # import csv
 import os
 
-
 COLS_TO_USE = [
     'CASE_NUMBER',
     'VISA_CLASS',
@@ -25,47 +24,21 @@ CSV_FILENAME = 'test'
 
 
 def csv_to_df(csv_filename):
-
     # Convert excel to csv
-    try:
-        # Try reading excel file with types enforced for each column
-        df = pd.read_excel(
-            f'data/{csv_filename}.xlsx',
-            usecols=COLS_TO_USE,
-            dtype={
-                'CASE_NUMBER': str,
-                'VISA_CLASS': str,
-                'JOB_TITLE': str,
-                'SOC_TITLE': str,
-                'FULL_TIME_POSITION': str,
-                'EMPLOYER_NAME': str,
-                'EMPLOYER_CITY': str,
-                'EMPLOYER_STATE': str,
-                'WAGE_RATE_OF_PAY_FROM': float,
-                'WAGE_RATE_OF_PAY_TO': float,
-                'WAGE_UNIT_OF_PAY': str,
-                'PREVAILING_WAGE': float,
-                'PW_UNIT_OF_PAY': str,
-            }
-        )
-        df = get_cleaned_dataframe(df)
-    except ValueError:
-        print('there was an error')
-        # If an error occurs because of a cell not matching the column type,
-        # (such as when a cell containing a string is in a column of floats)
-        # then read normally and do data cleaning to remove bad rows
-        df = pd.read_excel(
-            f'data/{csv_filename}.xlsx',
-            usecols=COLS_TO_USE,
-        )
-        df = get_cleaned_dataframe(df)
+    df = pd.read_excel(
+        f'data/{csv_filename}.xlsx',
+        usecols=COLS_TO_USE,
+    )
+    df = coerce_to_numeric_type(df)
+    df = strip_and_standardize_casing(df)
+    df['WAGE_RATE_OF_PAY_FROM'] = df['WAGE_RATE_OF_PAY_FROM'].astype(float)
+    df['WAGE_RATE_OF_PAY_TO'] = df['WAGE_RATE_OF_PAY_TO'].astype(float)
+    df['PREVAILING_WAGE'] = df['PREVAILING_WAGE'].astype(float)
 
-        # TODO: Convert types after cleaning.
-        #df['CASE_NUMBER'] = df['CASE_NUMBER'].astype(str)
-        # ...
     return df
 
-def get_cleaned_dataframe(df):
+
+def coerce_to_numeric_type(df):
     #̶ T̶O̶D̶O̶:̶ C̶h̶a̶n̶g̶e̶ t̶o̶ l̶i̶s̶t̶ c̶o̶m̶p̶r̶e̶h̶e̶n̶s̶i̶o̶n̶
     # List comprehension below doesn't work because list comprehension turns this into a list and not an object as required to mutate an entire dataframe
     # df = [df.iloc[:, row].str.strip() for row in range(len(df.columns)) if df.iloc[:, row].dtype == 'object']
@@ -75,6 +48,10 @@ def get_cleaned_dataframe(df):
     df['WAGE_RATE_OF_PAY_FROM'] = pd.to_numeric(df['WAGE_RATE_OF_PAY_FROM'], errors='coerce')
     df['WAGE_RATE_OF_PAY_TO'] = pd.to_numeric(df['WAGE_RATE_OF_PAY_TO'], errors='coerce')
 
+    return df
+
+
+def strip_and_standardize_casing(df):
     # Get rid of all the spaces and standardize casing for strings
     # Create an object out of all the columns with the dtype object
     df_object = df.select_dtypes(['object'])
@@ -84,6 +61,7 @@ def get_cleaned_dataframe(df):
     df[df_object.columns] = df_object.apply(lambda x: x.str.strip()).apply(lambda x: x.str.title())
 
     return df
+
 
 def connect_to_database():
     """
@@ -102,6 +80,7 @@ def connect_to_database():
     engine = sqlalchemy.create_engine(connection_string)
     return engine
 
+
 def export_dataframe_to_database(df, engine, table_name):
     """
     Export dataframe to database
@@ -109,11 +88,12 @@ def export_dataframe_to_database(df, engine, table_name):
         df: pandas dataframe
         engine: sqlalchemy engine
         table_name: name of table to be created in database
-    Return: N/A 
+    Return: N/A
     """
     print("Exporting dataframe to database...")
     df.to_sql(f'{table_name}', engine, if_exists='replace', index=False)
     print("Dataframe exported to database successfully!")
+
 
 # TODO: Remove the helper function later
 def create_small_dataframe():
@@ -123,11 +103,12 @@ def create_small_dataframe():
     TO BE REMOVED LATER
     """
     values= [['Rohan',455],['Elvish',250],['Deepak',495],
-            ['Soni',400],['Radhika',350],['Vansh',450]] 
-    
+            ['Soni',400],['Radhika',350],['Vansh',450]]
+
     df = pd.DataFrame(values,columns=['Name','Total_Marks'])
     df = df.assign(Percentage = lambda x: (x['Total_Marks'] /500 * 100))
     return df
+
 
 if __name__ == '__main__':
 
@@ -135,7 +116,7 @@ if __name__ == '__main__':
     load_dotenv()
 
     # Create a dataframe from the csv file
-    # df = csv_to_df(CSV_FILENAME)
+    df = csv_to_df(CSV_FILENAME)
 
     # Connect to database
     engine = connect_to_database()
@@ -146,13 +127,13 @@ if __name__ == '__main__':
 
     # Export the dataframe to the database
     export_dataframe_to_database(test_df, engine, "exam_score")
-    
+
     # TODO: remove prints below later
     # Print tables in the database
     metadata = sqlalchemy.MetaData()
     metadata.reflect(bind=engine)
     print(metadata.tables)
-    
+
     # Print all rows in table
     with engine.connect() as conn:
         result = conn.execute(

e04940c7034a67764acb552a671e199794429ba7
Allen <allenjhuang92@gmail.com>
2022-01-21 18:32:25 -0600

    Use dictionary cursor

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from flask import Flask, jsonify
 import psycopg2
+import psycopg2.extras
 import os
 
 app = Flask(__name__)
@@ -10,8 +11,8 @@ load_dotenv()
 @app.route("/")
 def data():
     conn = psycopg2.connect(os.environ["CRDB_CONN_STR"])
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM exam_score")
+    with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+        cur.execute("SELECT * FROM h1bdata_table")
         rows = cur.fetchall()
         conn.commit()
     return jsonify(rows)
